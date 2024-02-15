4157c1e8ade435ee9bc45b1ffe6f80e7752616bd
volodyabuz <volodyabuz@gmail.com>
2024-02-15 18:21:57 +0300

    new_db_created

--- audiapi/admin.py
+++ audiapi/admin.py
@@ -1,22 +1,39 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Cars)
-
-
 
 admin.site.register(Garage)
 admin.site.register(Recommendations)
 admin.site.register(Comments)
 
-class GearboxesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'gear', 'wd')
-admin.site.register(Gearboxes, GearboxesAdmin)
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'generation', 'body_type', 'fuel_type', 'motor', 'gearbox', 'wheel_drive', 'about', 'car_photo')
+admin.site.register(Cars, CarsAdmin)
+
+class CarModelsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car_name')
+admin.site.register(CarModels, CarModelsAdmin)
 
 class GenerationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'car_name', 'gen', 'body')
+    list_display = ('id', 'model_name', 'gen')
 admin.site.register(Generations, GenerationsAdmin)
 
+class BodyTypesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'body')
+admin.site.register(BodyTypes, BodyTypesAdmin)
+
+class FuelTypesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fuel')
+admin.site.register(FuelTypes, FuelTypesAdmin)
+
 class MotorsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'gen', 'fuel', 'engine', 'gear')
+    list_display = ('id', 'gen', 'engine')
 admin.site.register(Motors, MotorsAdmin)
+
+class GearboxesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'gear')
+admin.site.register(Gearboxes, GearboxesAdmin)
+
+class WheelDrivesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'wd')
+admin.site.register(WheelDrives, WheelDrivesAdmin)
--- audiapi/models.py
+++ audiapi/models.py
@@ -1,92 +1,153 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
+
 
 #Классы об автомобилях
 
 class Cars(models.Model):
     '''Конкретный автомобиль из существующих.'''
 
-    name = models.ForeignKey('Generations', on_delete=models.CASCADE, verbose_name='Модель')
+    name = models.ForeignKey('CarModels', on_delete=models.CASCADE, verbose_name='Модель')
+    generation = models.ForeignKey('Generations', on_delete=models.CASCADE, verbose_name='Поколение')
+    body_type = models.ForeignKey('BodyTypes', on_delete=models.CASCADE, verbose_name='Тип кузова')
+    fuel_type = models.ForeignKey('FuelTypes', on_delete=models.CASCADE, verbose_name='Топливо')
+    motor = models.ForeignKey('Motors', on_delete=models.CASCADE, verbose_name='Двигатель')
+    gearbox = models.ForeignKey('Gearboxes', on_delete=models.CASCADE, verbose_name='Коробка передач')
+    wheel_drive = models.ForeignKey('WheelDrives', on_delete=models.CASCADE, verbose_name='Привод')
     about = models.TextField(blank=True, verbose_name='Об автомобиле')
-    motor = models.ForeignKey('Motors', on_delete=models.CASCADE)
-    gearbox = models.ForeignKey('Gearboxes', on_delete=models.CASCADE)
-    car_photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото автомобиля')
+    car_photo = models.ImageField(upload_to="car_photos/%Y/%m/%d/", verbose_name='Фото автомобиля')
 
     def __str__(self):
-        return self.name
+        return f'{self.name}, {self.generation}'
 
     class Meta:
-        verbose_name = 'Автомобили'
+        verbose_name = 'автомобиль'
         verbose_name_plural = 'Автомобили'
+
+
+class CarModels(models.Model):
+    '''Модель автомобиля.'''
+
+    car_name = models.CharField(max_length=50, db_index=True, verbose_name='Модель')
+
+    def __str__(self) -> str:
+        return self.car_name
+
+    class Meta:
+        verbose_name = 'модель Audi'
+        verbose_name_plural = 'Модели Audi'
+        ordering = ['car_name',]
 
 
 class Generations(models.Model):
     '''Поколения автомобилей.'''
 
-    BODY_TYPE_CHOICES = [
-        ('HT3', 'Хэтчбек 3дв.'),
-        ('HT5', 'Хэтчбек 5дв.'),
-        ('SD4', 'Седан 4дв.'),
-        ('UV5', 'Универсал 5дв.'),
-        ('SV5', 'Внедорожник 5дв.'),
-        ('CU2', 'Купэ 2дв.'),
-        ('CB2', 'Кабриолет 2дв.'),
-    ]
-
-    car_name = models.CharField(max_length=50, db_index=True, verbose_name='Модель')
+    model_name = models.ForeignKey('CarModels', on_delete=models.CASCADE, verbose_name='Модель')
     gen = models.CharField(max_length=50, verbose_name='Поколение')
-    body = models.CharField(max_length=3, choices=BODY_TYPE_CHOICES, verbose_name='Тип кузова')
-
 
     def __str__(self) -> str:
-        return self.gen
+        return f'{self.model_name}, {self.gen}'
 
     class Meta:
-        verbose_name = 'Поколения автомобилей'
-        verbose_name_plural = 'Поколения автомобилей'
+        verbose_name = 'поколение'
+        verbose_name_plural = 'Поколения'
+
+
+class BodyTypes(models.Model):
+    '''Типы кузова автомобиля.'''
+
+    # BODY_TYPE_CHOICES = [
+    #     ('HT3', 'Хэтчбек 3дв.'),
+    #     ('HT5', 'Хэтчбек 5дв.'),
+    #     ('SD4', 'Седан 4дв.'),
+    #     ('UV5', 'Универсал 5дв.'),
+    #     ('SV5', 'Внедорожник 5дв.'),
+    #     ('CU2', 'Купэ 2дв.'),
+    #     ('CB2', 'Кабриолет 2дв.'),
+    # ]
+
+    body = models.CharField(max_length=20, verbose_name='Тип кузова')
+
+    def __str__(self) -> str:
+        return self.body
+
+    class Meta:
+        verbose_name = 'тип кузова'
+        verbose_name_plural = 'Типы кузова'
+
+
+class FuelTypes(models.Model):
+    '''Типы топлива двигателя.'''
+
+    # FUEL_CHOICES = [
+    #     ("PT", 'Бензин'),
+    #     ("DT", 'Дизель'),
+    #     ("HB", 'Гибрид'),
+    #     ("EL", 'Электро')
+    # ]
+
+    fuel = models.CharField(max_length=10, default='Бензин', verbose_name='Тип двигателя')
+
+    def __str__(self) -> str:
+        return self.fuel
+
+    class Meta:
+        verbose_name = 'тип топлива'
+        verbose_name_plural = 'Типы топлива'
 
 
 class Motors(models.Model):
-    '''Типы двигателей их их модели.'''
+    '''Модели двигателя автомобиля.'''
 
-    FUEL_CHOICES = [
-        ("PT", 'Бензин'),
-        ("DT", 'Дизель'),
-        ("HB", 'Гибрид')
-    ]
-
-    gen = models.ForeignKey('Generations', on_delete=models.CASCADE)
-    fuel = models.CharField(max_length=2, choices=FUEL_CHOICES, default=FUEL_CHOICES[0], verbose_name='Тип двигателя')
+    gen = models.ForeignKey('Generations', on_delete=models.CASCADE, verbose_name='Поколение')
     engine = models.CharField(db_index=True, verbose_name='Двигатель')
-    gear = models.ForeignKey('Gearboxes', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.engine
 
     class Meta:
-        verbose_name = 'Двигатели'
+        verbose_name = 'двигатель'
         verbose_name_plural = 'Двигатели'
+
 
 class Gearboxes(models.Model):
     '''Типы коробок передач.'''
 
-    WHEELDRIVE_CHOICES = [
-        ("FWD", 'Передний'),
-        ("RWD", 'Задний'),
-        ("AWD", 'Полный')
-    ]
+    # GEARBOX_CHOICES = [
+    #     ("MT", 'Механика'),
+    #     ("AT", 'Автомат'),
+    #     ("AMT", 'Робот'),
+    #     ("CVT", 'Вариатор')
+    # ]
 
-    gear = models.CharField(db_index=True, verbose_name='Коробка передач')
-    wd = models.CharField(max_length=3, choices=WHEELDRIVE_CHOICES, default=WHEELDRIVE_CHOICES[0], verbose_name='Привод')
+    gear = models.CharField(max_length=10, default='Механика', verbose_name='Коробка передач')
 
     def __str__(self) -> str:
         return self.gear
     
     class Meta:
-        verbose_name = 'Коробки передач'
+        verbose_name = 'коробку передач'
         verbose_name_plural = 'Коробки передач'
         ordering = ['id',]
+
+
+class WheelDrives(models.Model):
+    '''Типы привода автомобиля.'''
+
+    # WHEELDRIVE_CHOICES = [
+    #     ("FWD", 'Передний'),
+    #     ("RWD", 'Задний'),
+    #     ("AWD", 'Полный')
+    # ]
+
+    wd = models.CharField(max_length=10, default='Передний', verbose_name='Привод')
+
+    def __str__(self) -> str:
+        return self.wd
+
+    class Meta:
+        verbose_name = 'тип привода'
+        verbose_name_plural = 'Типы привода'
 
 
 #Классы о пользователях
@@ -108,7 +169,8 @@ class Recommendations(models.Model):
 
     car = models.ForeignKey('Cars', on_delete=models.CASCADE)
     url = models.TextField(blank=True, verbose_name='Объявление')
-    time_shared = models.DateTimeField(auto_now=True, verbose_name='Время размещения')
+    url_photo = models.ImageField(upload_to="url_photos/%Y/%m/%d/", verbose_name='Фото объявления')
+    shared_time = models.DateTimeField(auto_now=True, verbose_name='Время размещения')
 
     class Meta:
         verbose_name = 'Рекомендации'
@@ -121,7 +183,7 @@ class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     car = models.ForeignKey('Cars', on_delete=models.CASCADE)
     comment = models.TextField(blank=True, verbose_name='Комментарий')
-    users_photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото от пользователя')
+    user_photo = models.ImageField(upload_to="user_photos/%Y/%m/%d/", verbose_name='Фото от пользователя')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время публикации')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
