7d19028b847287b35bc7a95d25cc121d9c0c53ce
volodyabuz <volodyabuz@gmail.com>
2024-03-21 23:59:53 +0300

    ModelViewSet and SimpleRouter

--- audiapi/urls.py
+++ audiapi/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'audi', AudiViewSet)
 
 urlpatterns = [
     path('', get_car_model_list, name='home'),
+    path('api/v1/', include(router.urls)),
     # path('get-model-car-list/<int:car_id>/', AudiAPIList.as_view(), name='get-model-car-list'),
-    path('api/v1/audilist/', AudiAPIList.as_view()),
-    path('api/v1/audilist/<int:pk>/', AudiAPIUpdate.as_view()),
-    path('api/v1/audidetail/<int:pk>/', AudiAPIDetailView.as_view()),
+    # path('api/v1/audilist/', AudiViewSet.as_view({'get': 'list'})),
+    # path('api/v1/audilist/<int:pk>/', AudiViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
 ]
--- audiapi/views.py
+++ audiapi/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .forms import CarForm
 
@@ -17,69 +18,23 @@ def get_car_model_list(request):
     context = {'title': 'Главная', 'car_list': car_list, 'model_car_list': model_car_list}
     return render(request, 'audiapi/index.html', context=context)
 
-# class AudiAPIView(generics.ListAPIView):
+
+class AudiViewSet(viewsets.ModelViewSet):
+    queryset = Cars.objects.all()
+    serializer_class = AudiSerializer
+
+
+# class AudiAPIList(generics.ListCreateAPIView):
 #     queryset = Cars.objects.all()
 #     serializer_class = AudiSerializer
 
 
-class AudiAPIList(generics.ListCreateAPIView):
-    queryset = Cars.objects.all()
-    serializer_class = AudiSerializer
+# class AudiAPIUpdate(generics.UpdateAPIView):
+#     queryset = Cars.objects.all()
+#     serializer_class = AudiSerializer
 
 
-class AudiAPIUpdate(generics.UpdateAPIView):
-    queryset = Cars.objects.all()
-    serializer_class = AudiSerializer
+# class AudiAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cars.objects.all()
+#     serializer_class = AudiSerializer
 
-
-class AudiAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cars.objects.all()
-    serializer_class = AudiSerializer
-
-# class AudiAPIView(APIView):
-
-#     def get(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if pk:
-#             try:
-#                 instance = Cars.objects.get(pk=pk)
-#                 return Response({'posts': AudiSerializer(instance).data})
-#             except:
-#                 return Response({'error': 'Object does not exist'})
-#         lst = Cars.objects.all()
-#         return Response({'posts': AudiSerializer(lst, many=True).data})
-
-    
-#     def post(self, request):
-#         serializer = AudiSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-#         return Response({'new_car': serializer.data})
-
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({'error': 'Method PUT is not allowed'})
-        
-#         try:
-#             instance = Cars.objects.get(pk=pk)
-#         except:
-#             return Response({'error': 'Object does not exist'})
-
-#         serializer = AudiSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'changed_car': serializer.data})
-    
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk')
-#         if not pk:
-#             return Response({'error': 'Method DELETE is not allowed'})
-        
-#         try:
-#             temp = Cars.objects.get(pk=pk)
-#         except:
-#             return Response({'error': 'Object does not exist'})
-#         temp.delete()
-#         return Response({'deleted_item': AudiSerializer(temp).data})
