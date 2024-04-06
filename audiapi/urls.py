cbcffeb62599febfca2ee5d65049e232da3b83de
volodyabuz <volodyabuz@gmail.com>
2024-04-07 01:25:36 +0300

    changed Djoser to JWT

--- audiapi/urls.py
+++ audiapi/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from .views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 router = routers.DefaultRouter()
@@ -17,6 +18,9 @@ urlpatterns = [
     path('get-model-car-list/<int:pk>/', AudiViewSet.as_view({'get': 'retrieve'}), name='get-model-car-list'),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/audilist/', AudiViewSet.as_view({'get': 'list'})),
     # path('api/v1/audilist/<int:pk>/', AudiViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
 ]
